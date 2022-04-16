a3536533ca1896d9da152ce60ca98c0e82931b8c
Nitish Naskar <naskarnitish18@gmail.com>
2022-04-16 15:30:32 +0530

    add ejs and mongodb

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const express=require("express");
 const app=express();
 const port=8000;
+const db=require('./config/mongoose');
+const Contact=require('./models/todolist');
+
+app.set("view engine","ejs");
+app.use(express.static("assets"));
+
+app.get('/',function(req,res){
+    return res.render("index",{
+    });
+})
+
+app.listen(port,function(err){
+    if(err){
+        console.log("Some error in this surver >> ",err);
+        return;
+    }
+
+    console.log("OK ! your server is running ......");
+});
